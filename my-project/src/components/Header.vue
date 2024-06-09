9657fe5c1539ced94ec30ed2d11d437542716a57
Geordani <geordanimachado756@gmail.com>
2024-06-09 15:29:04 -0300

    food App

---
+++ my-project/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="p-2 justify-between flex">
+        <div class="flex">
+            <img src="https://i.pinimg.com/originals/8a/b6/d7/8ab6d70f03197bf092b242083cc1c4d6.png" alt="Logo" class="h-12"/>
+            <hi class="mt-2 font-sans font-black p-1">Açai no Pote</hi>
+        </div>
+        <div>
+            Home | About | Contact
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    mounted() {
+        document.title = 'Header';
+    }
+}
+</script>
---
+++ my-project/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Header/>
+        <h1>Home</h1>
+        <p>Welcome to the Home page</p>
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+
+export default {
+    name: 'Home',
+    mounted() {
+        document.title = 'Home';
+    },
+    components: {
+        Header
+    }
+}
+</script>
